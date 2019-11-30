ee6dbc7fb9482b29db5e805dd6833b315b9f36b2
dtcookie <41952460+dtcookie@users.noreply.github.com>
2019-11-30 09:28:51 +0100

    j

--- weihnachts-vorgluehen/index.js
+++ weihnachts-vorgluehen/index.js
@@ -1,12 +1,22 @@
 var images = ["Gluehwein.jpg", "hoftaverne.jpg"];
 var idx = 0;
+var opacity = 100;
+var delta = -10;
 
 function roll() {
-    idx++;
-    if (idx == images.length) {
-        idx = 0;
+    opacity = opacity + delta;
+    if (opacity > 100) {
+        delta = -delta;
+        opacity = 100 + delta;
+    } else if (opacity < 0) {
+        delta = -delta;
+        opacit = delta;
+        idx++;
+        if (idx == images.length) {
+            idx = 0;
+        }
+        document.getElementById("rollover").src = images[idx];
     }
-    document.getElementById("rollover").src = images[idx];
     window.setTimeout(roll, 1000);
 }
 
